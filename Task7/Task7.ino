c33b980ef865fb5137a3a45d4e8f8797758c0bd3
DavidRhysGemmell <58792916+DavidRhysGemmell@users.noreply.github.com>
2022-02-24 20:14:38 +0000

    Add files via upload

---
+++ Task7/Task7.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (average_analogue_in > half of maximum range for 
+analogue input){
+    error_code = 1
+}else{
+    error_code = 0
+}
+}
