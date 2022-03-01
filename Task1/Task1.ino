099bf13ead523a38b6a4d727255d7507eb709da4
DavidRhysGemmell <58792916+DavidRhysGemmell@users.noreply.github.com>
2022-03-01 18:03:45 +0000

    Update Task1.ino

--- Task1/Task1.ino
+++ Task1/Task1.ino
@@ -1,16 +1,14 @@
-#define WaveformCycle 33750 // Length of each waveform cycle, calculated manually myself in matlab, although could be implemented using a for loop to calculate the sum of pulses of a.
+#define GreenLED //insert green LED pin
 
 void setup() {
   // put your setup code here, to run once:
-
+pinMode(GreenLED, OUTPUT):
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   \\TASK 1\\
- if (currentMicros-PreviousMicrosWaveform<=50){ // Keeps Green LED on for the first 0-50 uS, does not depend on Switch Settings
-   digitalWrite(GreenLED,HIGH);
- } else {
-   digitalWrite(GreenLED,LOW);
- }
+digitalWrite(GreenLED,HIGH);
+delayMicroseconds(50);
+digitalWrite(GreenLED,LOW);
 }
