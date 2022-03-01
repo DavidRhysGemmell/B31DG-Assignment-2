04ea6d254d6aa1c6271805d611b94618165419b9
DavidRhysGemmell <58792916+DavidRhysGemmell@users.noreply.github.com>
2022-03-01 18:11:16 +0000

    Update Task7.ino

--- Task7/Task7.ino
+++ Task7/Task7.ino
@@ -3,12 +3,15 @@ void setup() {
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-if (average_analogue_in > half of maximum range for 
+void Task2 {
+  if (average_analogue_in > half of maximum range for 
 analogue input){
     error_code = 1
 }else{
     error_code = 0
 }
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
 }
