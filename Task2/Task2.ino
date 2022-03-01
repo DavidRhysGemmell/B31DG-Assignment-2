8f40b94a63a2e0db574f38623d8b4dfc44b169bc
DavidRhysGemmell <58792916+DavidRhysGemmell@users.noreply.github.com>
2022-03-01 20:59:34 +0000

    Add files via upload

---
+++ Task2/Task2.ino
@@ -0,0 +1,17 @@
+#define Button 22 //insert Button 1 pin
+int ButtonState=0
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(Button, INPUT);
+}
+
+void Task2 {
+   ButtonState=digitalRead(Button1); //Tells when Button is pressed
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
