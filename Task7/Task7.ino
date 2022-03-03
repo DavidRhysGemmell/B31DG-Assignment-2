a9a9a2f27ed1ccce832c5ad5fe4235e7c9bdcf5d
DavidRhysGemmell <58792916+DavidRhysGemmell@users.noreply.github.com>
2022-03-03 18:24:12 +0000

    Update Task7.ino

--- Task7/Task7.ino
+++ Task7/Task7.ino
@@ -1,9 +1,10 @@
+
 void setup() {
   // put your setup code here, to run once:
 
 }
 
-void Task2 {
+void Task7 {
   if (average_analogue_in > half of maximum range for 
 analogue input){
     error_code = 1
