f6ae01e1a710009c8f57db19a635d774a519e52f
DavidRhysGemmell <58792916+DavidRhysGemmell@users.noreply.github.com>
2022-03-03 18:22:52 +0000

    Update Task6.ino

--- Task6/Task6.ino
+++ Task6/Task6.ino
@@ -2,10 +2,13 @@ void setup() {
   // put your setup code here, to run once:
 
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+void Task6() {
 for (int i=0; i<=1000; i++){
   __asm__ __volatile__ ("nop");
 }
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
 }
