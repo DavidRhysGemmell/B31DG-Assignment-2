70874cbd31e0bdbbe55f402aebc5ee24b9ddc4eb
DavidRhysGemmell <58792916+DavidRhysGemmell@users.noreply.github.com>
2022-03-03 18:22:25 +0000

    Update Task5.ino

--- Task5/Task5.ino
+++ Task5/Task5.ino
@@ -1,4 +1,10 @@
 //Need to define things without them reseting.
+int Prev1AnaInput = 0;
+int Prev2AnaInput = 0;
+int Prev3AnaInput = 0;
+int Prev4AnaInput = 0;
+int AverageAnaInput = 0;
+
 
 
 void setup() {
@@ -10,7 +16,7 @@ void Task5(){
   Prev4AnaInput = Prev3AnaInput;
   Prev3AnaInput = Prev2AnaInput;
   Prev2AnaInput = Prev1AnaInput;
-  Prev1AnaInput = AnalogueInput;
+  Prev1AnaInput = AnalogueRead;
   AverageAnaInput = (Prev4AnaInput+Prev3AnaInput+Prev2AnaInput+Prev1AnaInput)/4;
 }
 void loop() {
