73315248a2347aaec7a67eeddf537c362a6e7315
DavidRhysGemmell <58792916+DavidRhysGemmell@users.noreply.github.com>
2022-03-07 13:37:03 +0000

    Update Task7.ino

--- Task7/Task7.ino
+++ Task7/Task7.ino
@@ -1,12 +1,11 @@
-
+int half_of_maximum_range_for_analogue_input = 0;
 void setup() {
   // put your setup code here, to run once:
 
 }
 
 void Task7 {
-  if (average_analogue_in > half of maximum range for 
-analogue input){
+  if (average_analogue_in > half_of_maximum_range_for_analogue_input){
     error_code = 1
 }else{
     error_code = 0
